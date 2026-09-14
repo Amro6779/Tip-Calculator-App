5ac6e1629474ff7535be5dd5c5f87b957872faa2
Amro6779 <mr487680@gmail.com>
2026-09-14 15:07:43 +0300

    add function for calculating tip

--- js/index.js
+++ js/index.js
@@ -1,16 +1,34 @@
-const billInput = document.getElementById("bill-input");
-const customTipInput = document.getElementById("custom-tip-input");
-const peopleInput = document.getElementById("people-number-input");
+const billInput = document.getElementById("bill-amount");
 const tipButton = document.querySelectorAll(".tip-button");
+const customTipInput = document.getElementById("custom-tip");
+const numberOfPeople = document.getElementById("people-number-input");
 const tipAmount = document.getElementById("number1");
 const total = document.getElementById("number2");
 const resetButton = document.getElementById("reset-button");
 
+let tipPercent = 0;
+
+tipButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    tipPercent = parseFloat(button.innerText);
+    calculateTipAmount();
+  });
+});
 
 
 
-tipButton.forEach((button)=>{
-    button.addEventListener("click" , ()=>{
-        alert(button.innerText);
-    })
-})
+billInput.addEventListener("input" , calculateTipAmount);
+numberOfPeople.addEventListener("input" , calculateTipAmount);
+customTipInput.addEventListener("input" , calculateTipAmount);
+
+function calculateTipAmount(){
+    if (customTipInput.value != "") {
+        tipPercent = Number(customTipInput.value);
+    }
+
+    let tipTotal = billInput.value * (tipPercent / 100);
+    let tipPerPerson = tipTotal / numberOfPeople.value;
+    let totalPerPerson = (Number(billInput.value) + tipTotal) / numberOfPeople.value
+    tipAmount.innerText = tipPerPerson.toFixed(2);
+    total.innerText = totalPerPerson.toFixed(2);
+};
